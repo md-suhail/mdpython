20eba71d00d034ca4ed8950920ddf7493a752534
md-suhail <31612181+md-suhail@users.noreply.github.com>
2017-09-11 11:40:19 +0530

    Add files via upload

---
+++ suhail6.py
@@ -0,0 +1,12 @@
+a=[-1,-2,-3,0,1,2,3]
+print(a)
+for i in a:
+    if(i<0):
+        print(i)
+        print("negative")
+    if(i>0):
+        print(i)
+        print("positive")
+    if(i==0):
+        print(i)
+        print("zero")
